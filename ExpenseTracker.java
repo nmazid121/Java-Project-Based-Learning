a6b7ca33dd9e0ed8a6cae0d596e634fb9f41438b
nmazid121 <nmazid121@gmail.com>
2024-02-09 21:45:31 -0500

    insertion, deletion, and showing the array

--- ExpenseTracker.java
+++ ExpenseTracker.java
@@ -1,34 +1,60 @@
+import java.util.Scanner;
+
 public class ExpenseTracker {
     
     
-    public static int addExpense(int moneyInput) {
-        return moneyInput;
-    }
+    public static void addExpense(int [] argsArray, int deposit) {
+        // insertion for the expense
+        int count = 0;
+        for (int i = 0; i < argsArray.length; i++) {
+            if (argsArray[i] != 0) {
+                count++;
+                }
+            }
+            argsArray[count] = deposit;
+            System.out.println("Added " + deposit + " to the expenses");
+        }
 
-    public static int deleteExpense(int deleteInput) {
-        return deleteInput;
+    public static void deleteExpense(int [] argsArray, int remove) {
+        // deletion for the expense
+        for (int i = remove-1; i < argsArray.length-1; i++) {
+            argsArray[i] = argsArray[i+1];
+        }
+
+        System.out.println("Removed Expense " + remove);
     }
 
     public static void showExpenses(int [] argsArray) {
+        // shows all the expenses that are not 0
         for (int i = 0; i < argsArray.length; i++) {
             int startCounter = i+1;
             if (argsArray[i] != 0) {
-                System.out.println("Expense " + startCounter + ": " + argsArray[i]);
+                System.out.println("Expense " + startCounter + " : " + argsArray[i]);
             }
         }
     }
 
     public static void main (String[] args) {
-       // int userInput = Integer.parseInt(args[0]);
-      //  int addedExpense = addExpense(userInput);
-     //   System.out.println(addedExpense);
-        
+        Scanner input = new Scanner(System.in);
+
         int [] argsArray = new int[50];
         for (int i = 0; i < args.length; i++) {
             argsArray[i] = Integer.parseInt(args[i]);
         }
 
-        // output each expense in the array
-        showExpenses(argsArray);
+        System.out.println("Add an expense?");
+        String answer = input.nextLine();
+        if (answer.equals("Yes") || answer.equals("yes")) {
+            System.out.println("How much would you like to deposit?");
+            addExpense(argsArray, input.nextInt());
+            showExpenses(argsArray);
+        } else if (answer.equals("no") || answer.equals("No")) {
+            System.out.println("What expense would you like to remove?");
+            deleteExpense(argsArray, input.nextInt());
+            showExpenses(argsArray);
+        } else {
+            System.out.println("Showing current expenses :");
+            showExpenses(argsArray);
+            }
+        } 
     }
-}
