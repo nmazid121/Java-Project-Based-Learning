08f0a03612a2782dd045def5eb731d2f31569d2d
nmazid121 <nmazid121@gmail.com>
2024-02-19 16:45:58 -0500

    linked lists

---
+++ LinkedLists/ContactManagement.java
@@ -0,0 +1,32 @@
+public class ContactManagement {
+
+    Node head;
+
+    public static class Node {
+        int data;
+        Node next;
+
+        Node (int d) {
+            data  = d;
+            next = null;
+        }
+
+        public int getNode() {
+            return data;
+        }
+
+      //   public Node addNode() {
+      //      return Node;
+      //  }
+    }
+
+
+    public static void main(String[] args) {
+        Node anode = new Node(10);
+        Node nodeTwo = new Node(500);
+        System.out.println(anode.getNode());
+        System.out.println(nodeTwo.getNode());
+        
+
+    }
+}
