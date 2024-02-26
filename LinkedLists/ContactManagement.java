c598bff7cfc68ef851834c66f0b966b8869ba4fb
nmazid121 <nmazid121@gmail.com>
2024-02-25 23:06:51 -0500

    l

--- LinkedLists/ContactManagement.java
+++ LinkedLists/ContactManagement.java
@@ -1,7 +1,8 @@
+import java.io.NotActiveException;
+
 public class ContactManagement {
 
-    Node head;
-
+    // creating the class for the Nodes
     public static class Node {
         int data;
         Node next;
@@ -14,19 +15,39 @@ public class ContactManagement {
         public int getNode() {
             return data;
         }
-
-      //   public Node addNode() {
-      //      return Node;
-      //  }
     }
+
+    Node head;
+
+    // constructor for the ContactManagement classs
+    public ContactManagement() {
+        this.head = null; // Initializing the head to null
+    }
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
+
+   
+   
+   
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        newNode.next = null;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+    
 
 
     public static void main(String[] args) {
-        Node anode = new Node(10);
-        Node nodeTwo = new Node(500);
-        System.out.println(anode.getNode());
-        System.out.println(nodeTwo.getNode());
-        
+        Node johnNode = new Node(5);
+        System.out.println(johnNode.getNode());
+        johnNode.addFirst(5);
 
     }
 }
